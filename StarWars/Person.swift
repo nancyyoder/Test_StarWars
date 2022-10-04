b9c3ae78b76cee40c6f32dccfcf6b76b1661820d
Nancy Yoder <nayo4039@colorado.edu>
2022-10-04 11:07:22 -0600

    read json for person

--- StarWars/Person.swift
+++ StarWars/Person.swift
@@ -7,11 +7,40 @@
 
 import Foundation
 
-struct Person {
+struct Person: Codable {
     var name: String
     
     static var luke: Person{
-        Person(name: "Luke Skywalker");
+        func readLocalFile(forName name: String) -> Data? {
+            do {
+                if let bundlePath = Bundle.main.path(forResource: name,
+                                                     ofType: "json"),
+                    let jsonData = try String(contentsOfFile: bundlePath).data(using: .utf8) {
+                    return jsonData
+                }
+            } catch {
+                print("Could not read json file.", error)
+            }
+            
+            return nil
+        }
+        
+        if let data = readLocalFile(forName: "luke") {
+            print("we got data")
+            
+            let decoder = JSONDecoder()
+
+            do {
+                let luke = try decoder.decode(Person.self, from: data)
+                print("Here's Luke's Name: ", luke.name)
+                return luke
+            } catch {
+                print("Could not make person from data.", error.localizedDescription)
+            }
+        }
+        
+        
+        return Person(name: "Could not parse file.")
     }
 //    var height: Int
 //    var mass: Int
